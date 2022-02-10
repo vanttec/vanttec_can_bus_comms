8935fa415876a6156994552ca6d005eb29251bfe
Abiel <empoleom@gmail.com>
2022-02-09 21:41:54 -0600

    Multi threading fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <sstream>
 #include <ros/ros.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float32MultiArray.h>
@@ -9,6 +10,7 @@
 #include "Vanttec_CANLib_Linux/CANHandler.h"
 
 std::vector<float> lastArray{0,0,0,0,0,0,0,0};
+std::stringstream debugLogBuffer;
 
 void motorCallback(vanttec::CANHandler &handler, const std_msgs::Float32MultiArrayConstPtr& msg){
     if(msg->data.size() != 8)
@@ -16,6 +18,7 @@ void motorCallback(vanttec::CANHandler &handler, const std_msgs::Float32MultiArr
 
     for(int i = 0; i < msg->data.size(); i++){
         if(msg->data[i] == lastArray[i]) continue;
+        ROS_INFO("Sending motor message");
         vanttec::CANMessage canMsg;
         uint8_t id = 0x15 + i;
         vanttec::packFloat(canMsg, id, msg->data[i]);
@@ -23,6 +26,25 @@ void motorCallback(vanttec::CANHandler &handler, const std_msgs::Float32MultiArr
     }
 
     lastArray = msg->data;
+}
+
+void handleDebugMsg(can_frame frame){
+    if (frame.can_dlc - 1 <= 0)
+        return;
+    if(debugLogBuffer.str().size() > 10000){
+        ROS_ERROR("Debug log has not been flushed with \n!, Clearing buffer...");
+        debugLogBuffer.str(std::string()); //Clear buffer
+    }
+    for(int i = 1; i < frame.can_dlc; i++){
+        char newChar = frame.data[i];
+        if(newChar == '\n'){
+            //Flush current buffer
+            ROS_INFO("%s", debugLogBuffer.str().c_str());
+            debugLogBuffer.str(std::string()); //Clear buffer
+        } else {
+            debugLogBuffer << newChar;
+        }
+    }
 }
 
 int main(int argc, char **argv) {
@@ -35,25 +57,29 @@ int main(int argc, char **argv) {
         ROS_INFO("Recieved CAN MSG from id %d", id);
     };
 
-    handler.register_parser(can_bus_ros_parser);
+    //handler.register_parser(can_bus_ros_parser);
 
-    auto can_bus_parser = [&](uint8_t id, can_frame frame){
-        return;
-    };
+    handler.register_parser(0x14, handleDebugMsg);
 
-    handler.register_parser(can_bus_parser);
+    // auto can_bus_parser = [&](uint8_t id, can_frame frame){
+    //     if(id == 0x14) handleDebugMsg(frame);
+    // };
+
+    // //handler.register_parser(can_bus_parser);
+
+    std::atomic<bool> runThreads{true};
 
     std::thread update_read_thread([&]{
-        while(ros::ok()) handler.update_read();
+        while(runThreads.load() && ros::ok()) handler.update_read();
     });
 
     std::thread update_write_thread([&]{
-        while(ros::ok()) handler.update_write();
+        while(runThreads.load() && ros::ok()) handler.update_write();
     });
 
     std::thread hb_write_thread([&]{
         ros::Rate rate(1); //1Hz
-        while(ros::ok()){
+        while(runThreads.load() && ros::ok()){
             vanttec::CANMessage msg;
             msg.len = can_pack_byte(0x1E, 0xFF, msg.data, msg.len);
             handler.write(msg);
@@ -75,6 +101,10 @@ int main(int argc, char **argv) {
     });
 
     ros::spin();
+    runThreads.store(true);
+    update_read_thread.join();
+    update_write_thread.join();
+    hb_write_thread.join();
 
     return 0;
 }
