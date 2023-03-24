e2c47345c9b759e0e9cfe0c9378eae49131ffb85
Abiel Fernandez <empoleom@gmail.com>
2023-03-23 18:35:46 -0600

    Updated individual thrusters node

--- src/individual_thrusters.cpp
+++ src/individual_thrusters.cpp
@@ -12,51 +12,43 @@
 float left_thrust = 0;
 float right_thrust = 0;
 
-void leftThrustCallback(const std_msgs::Float32::ConstPtr& left, ros::Publisher &pub)
-{
-  std_msgs::Float32MultiArray msg;
-  left_thrust = left->data;
-
-  if(left_thrust > 36.5){
-    left_thrust = 1;
-  } else if(left_thrust < -30){
-    left_thrust = -1;
-  } else if(left_thrust > 0){
-    left_thrust = left_thrust / 36.5;
-  } else{
-    left_thrust = left_thrust / 30;
-  }
-
-  msg.data = std::vector<float>{left_thrust, right_thrust, 0,0,0,0,0,0};
-  pub.publish(msg);
-}
-
-void rightThrustCallback(const std_msgs::Float32::ConstPtr& right, ros::Publisher &pub)
-{
-  std_msgs::Float32MultiArray msg;
-  right_thrust = right->data;
-
-  if(right_thrust > 36.5)
-    right_thrust = 1;
-  else if(right_thrust < -30)
-    right_thrust = -1;
-  else if(right_thrust > 0)
-    right_thrust/=36.5;
+float map_thruster(float in){
+  if(in > 36.5)
+    in = 1;
+  else if(in < -30)
+    in = -1;
+  else if(in > 0)
+    in /= 36.5;
   else
-    right_thrust/=30;
+    in /= 30;
+  return in;
+}
 
+void update_thrust(ros::Publisher &pub, float left_thrust, float right_thrust){
+  std_msgs::Float32MultiArray msg;
   msg.data = std::vector<float>{left_thrust, right_thrust, 0,0,0,0,0,0};
   pub.publish(msg);
 }
 
+void leftThrustCallback(const std_msgs::Float32::ConstPtr& left, ros::Publisher &pub){
+  std_msgs::Float32MultiArray msg;
+  left_thrust = map_thruster(left->data);
+  update_thrust(pub, left_thrust, right_thrust);
+}
 
-int main(int argc, char **argv)
-{
+void rightThrustCallback(const std_msgs::Float32::ConstPtr& right, ros::Publisher &pub){
+  std_msgs::Float32MultiArray msg;
+  right_thrust = map_thruster(right->data);
+  update_thrust(pub, left_thrust, right_thrust);
+}
+
+
+int main(int argc, char **argv){
   ros::init(argc, argv, "individual_thrusters");
   ros::NodeHandle n;
-  ros::Publisher motors_pub = n.advertise<std_msgs::Float32MultiArray>("/motors", 1000);
-  auto left_sub = n.subscribe<std_msgs::Float32>("/motors/left_thrust", 1000, boost::bind(&leftThrustCallback, _1, boost::ref(motors_pub)));
-  auto right_sub = n.subscribe<std_msgs::Float32>("/motors/right_thrust", 1000, boost::bind(&rightThrustCallback, _1, boost::ref(motors_pub)));
+  ros::Publisher motors_pub = n.advertise<std_msgs::Float32MultiArray>("/motors", 10);
+  auto left_sub = n.subscribe<std_msgs::Float32>("/motors/left_thrust", 10, boost::bind(&leftThrustCallback, _1, boost::ref(motors_pub)));
+  auto right_sub = n.subscribe<std_msgs::Float32>("/motors/right_thrust", 10, boost::bind(&rightThrustCallback, _1, boost::ref(motors_pub)));
   ros::spin();
   return 0;
 }
